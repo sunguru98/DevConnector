2dee853f6989c49c62f81dea068d77855a4637c3
sundeep <sunguru98@yahoo.co.in>
2019-09-23 13:04:42 +0530

    Further bug fixes

--- server.js
+++ server.js
@@ -21,9 +21,9 @@ app.use('/api/auth', require('./routes/authRouter'))
 
 // Serve react (static)
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'))
+  app.use(express.static(path.join(__dirname, "client", "build")))
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
   })
 }
 
